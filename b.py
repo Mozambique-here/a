28351d4ba716a3c8ffedf474e38a66354b210820
Mozambique-here <56603183+Mozambique-here@users.noreply.github.com>
2019-10-15 22:14:06 +0300

    Add files via upload

--- b.py
+++ b.py
@@ -2,12 +2,15 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow,  QMessageBox, QLabel
 from PyQt5 import uic
 from math import *
+from PyQt5.QtGui import QIcon
  
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('start.ui',self)
-
+        self.setWindowTitle('A-calc')  
+        self.setWindowIcon(QIcon('a.png')) 
+        
         self.pushButton1.clicked.connect(self.run)
         self.pushButton2.clicked.connect(self.run)
         self.pushButton3.clicked.connect(self.run)
@@ -37,6 +40,7 @@ class MyWidget(QMainWindow):
         s = self.label.text()
    
         self.label.setText(s + n)
+        self.label_3.setText(s + n)
         
         try:
             self.label_2.setText(str(round(eval(s+n), 10)))
@@ -49,40 +53,62 @@ class MyWidget(QMainWindow):
     def dell(self):
         self.label.setText('')
         self.label_2.setText('')
+        self.label_3.setText('')
         
     def eq(self):
         s = str(self.label.text())
         try:
             self.label.setText(str(round(eval(s), 10)))
             self.label_2.setText(str(round(eval(s), 10)))
+            self.label_3.setText(str(round(eval(s), 10)))
         except:
-            self.label.setText('ERROR')
+            self.label_3.setText('ERROR')
     
     def keyPressEvent(self, event):
 
         if event.text() == '\r':
             s = str(self.label.text())
             try:
-                if type(eval(s)) == int:
-                    self.label.setText(str(round(eval(s), 10)))
-                    print(1)
+                self.label.setText(str(round(eval(s), 10)))
+                self.label_3.setText(str(round(eval(s), 10)))
+
             except:
-                self.label.setText('ERROR')
+                self.label_3.setText('ERROR')
                 
             
         if event.text() == '':
             s = str(self.label.text())
             try:
-                self.label.setText(s[:-1])
-                self.label_2.setText(s[:-1])
+                if len(s) == 1:
+                    self.label.setText('')
+                    self.label_2.setText('')
+                    self.label_3.setText('')                    
+                    
+                if s[-2] in 'snng':
+                    print(s)
+                    if len(s) > 4 and s[-5] in 'a' :
+                        s = s[:-5]
+                        print(1)
+                    else:
+                        s = s[:-4]
+                        print(2)
+                else:
+                    s = s[:-1]
+                    print(3)
+                self.label.setText(s)
+                self.label_2.setText(s)
+                self.label_3.setText(s)
+                
 
             except:
-                self.label.setText('ERROR')
+                #self.label_3.setText('ERROR')
+                pass
                 
         if event.text() in '1234567890)(*/+-//%**.':
             n = event.text()
             s = self.label.text()
-            self.label.setText(s + n)        
+            self.label.setText(s + n)
+            self.label_3.setText(s + n) 
             
             try:
                 self.label_2.setText(str(round(eval(s+n), 10)))
@@ -90,29 +116,15 @@ class MyWidget(QMainWindow):
                 self.label_2.setText('')
                 print(2)
             
-        #if event.text() not in '1234567890)(*/+-//%**.\r':
-            #self.label_2.setText('')
-            #self.label.setText('')
-                
-        #if event.key() == 92:
-            #self.label.setText('')
-            #try:
-                #self.label_2.setText(str(round(eval(s+n), 10)))
-            #except:
-                #self.label_2.setText('')
-                
-        #if event.key() == 93:
-            #s = str(self.label.text())[:-1]
-            #self.label.setText(s)
-            #try:
-                #self.label_2.setText(str(round(eval(s+n), 10)))
-            #except:
-                #self.label_2.setText('')
             
-        if event.text() == 's':
-            n = 'sin('
+        if event.text() == 's' or event.text() == 'S':
+            if event.text() == 's':
+                n = 'sin('
+            else:
+                n='asin('
             s = self.label.text()
-            self.label.setText(s + n)            
+            self.label.setText(s + n)
+            self.label_3.setText(s + n) 
             
             try:
                 self.label_2.setText(str(round(eval(s+n), 10)))
@@ -120,10 +132,14 @@ class MyWidget(QMainWindow):
                 self.label_2.setText('')
         
         
-        if event.text() == 'c':
-            n = 'cos('
+        if event.text() == 'c' or event.text() == 'C':
+            if event.text() == 'c':
+                n = 'cos('
+            else:
+                n='acos('
             s = self.label.text()
-            self.label.setText(s + n)            
+            self.label.setText(s + n)
+            self.label_3.setText(s + n) 
             
             try:
                 self.label_2.setText(str(round(eval(s+n), 10)))
@@ -131,10 +147,14 @@ class MyWidget(QMainWindow):
                 self.label_2.setText('')
                 
         
-        if event.text() == 't':
-            n = 'tan('
+        if event.text() == 't' or event.text() == 'T':
+            if event.text() == 't':
+                n = 'tan('
+            else:
+                n='atan('
             s = self.label.text()
-            self.label.setText(s + n)            
+            self.label.setText(s + n)
+            self.label_3.setText(s + n) 
             
             try:
                 self.label_2.setText(str(round(eval(s+n), 10)))
@@ -144,7 +164,8 @@ class MyWidget(QMainWindow):
         if event.text() == 'l':
             n = 'log('
             s = self.label.text()
-            self.label.setText(s + n)            
+            self.label.setText(s + n)
+            self.label_3.setText(s + n) 
             
             try:
                 self.label_2.setText(str(round(eval(s+n), 10)))
